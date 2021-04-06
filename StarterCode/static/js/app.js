b51dc5bda954a5a45c4f4df14de7b93044d47b09
Chelsea Chaussee <cfchaussee@gmail.com>
2021-04-05 19:55:31 -0700

    syntax will be the death of me

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,16 +2,25 @@ d3.json('samples.json').then(data => {
     console.log(data);
 })
 
-let trace1 = {
+function data_info () {
+
+let otu_ids = data.samples[0].otu_ids;
+console.log(otu_ids);
+let sample_values = data.samples[0].sample_values
+console.log(sample_values);
+let otu_labels = data.samples[0].otu_labels;
+console.log(otu_labels);
+};
+  let trace1 = {
     type: "bar",
     orientation: "h",
     x: sample_values,
     y: otu_ids, 
 }
 
-let data = [trace1];
+  let data = [trace1];
   
-var layout = {
+  let layout = {
   title: 'Top 10 Belly Button Samples',
   showlegend: false,
   height: 600,
@@ -20,10 +29,10 @@ var layout = {
 
 Plotly.newPlot('bar', data, layout);
 
-let trace2 = {
+  let trace2 = {
     x: otu_ids,
     y: sample_values,
-    text: ['A<br>size: 40', 'B<br>size: 60', 'C<br>size: 80', 'D<br>size: 100'],
+    text: otu_labels,
     mode: 'markers',
     marker: {
       color: otu_ids,
@@ -33,7 +42,7 @@ let trace2 = {
   
   let data2 = [trace2];
   
-  var layout2 = {
+  let layout2 = {
     title: 'Belly Button Bubble Chart',
     showlegend: false,
     height: 600,
