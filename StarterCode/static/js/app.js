f2ca8eb9b8103e8a3b9efc403bf62ff69e441519
Chelsea Chaussee <cfchaussee@gmail.com>
2021-04-07 19:10:55 -0700

    more pseudo code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,76 +1,56 @@
-
-// how my mind proccesses things
+//my mind can only understand if things are in a function
 function data_info () {
-d3.json('samples.json').then((data) => {
-    console.log(data);
-
-let otu_ids = data.samples[0].otu_ids;
-console.log(otu_ids);
-let sample_values = data.samples[0].sample_values
-console.log(sample_values);
-let otu_labels = data.samples[0].otu_labels;
-console.log(otu_labels)})};
-data_info();
-
-
-function init() {
-  var dropdown = d3.select("#selDataset");
-  d3.json("samples.json").then((data)=> {
-      console.log(data)
-      data.names.forEach(function(name) {
-          dropdown.append("option").text(name).property("value");
-      });
-      
-  let otu_ids = data.samples[0].otu_ids.slice(0,10).reverse();
-  let sample_values = data.samples[0].sample_values.slice(0,10).reverse();
+  d3.json('samples.json').then((data) => {
+      console.log(data);
+  let otu_ids = data.samples[0].otu_ids;
+  console.log(otu_ids);
+  let sample_values = data.samples[0].sample_values
+  console.log(sample_values);
   let otu_labels = data.samples[0].otu_labels;
-
-
-  let trace1 = {
-
-    x: sample_values,
-    y: otu_ids,
-    type: "bar",
-    orientation: "h" 
-}
-
-  let dataa = [trace1];
-  
-  let layout = {
-  title: 'Top 10 Belly Button Samples',
-  showlegend: false,
-  height: 600,
-  width: 600
-};
-
-Plotly.newPlot('bar', dataa, layout);
-
-  let trace2 = {
-    x: otu_ids,
-    y: sample_values,
-    text: otu_labels,
-    mode: 'markers',
-    marker: {
-      color: otu_ids,
-      size: sample_values
-    }
-};
-  
-  let data2 = [trace2];
-  
-  let layout2 = {
-    title: 'Belly Button Bubble Chart',
+  console.log(otu_labels)})};
+  data_info();
+  // Repeating getting the data because javascript wont reference things
+  function init() {
+    var dropdown = d3.select("#selDataset");
+    d3.json("samples.json").then((data)=> {
+        console.log(data)
+        data.names.forEach(function(name) {
+            dropdown.append("option").text(name).property("value");
+        });
+    let otu_ids = data.samples[0].otu_ids.slice(0,10).reverse();
+    let sample_values = data.samples[0].sample_values
+    let otu_labels = data.samples[0].otu_labels;
+    //creation of traces
+    let trace1 = {
+      x: sample_values,
+      y: otu_ids.slice(0,10).reverse(),
+      type: "bar",
+      orientation: "h" 
+  }
+    let dataa = [trace1];
+    let layout = {
+    title: 'Top 10 Belly Button Samples',
     showlegend: false,
     height: 600,
     width: 600
-};
-
-Plotly.newPlot('bubble', data2, layout2)})}
-
-function optionChanged(otu_ids) {
-  data_info(otu_ids);
-  init(otu_ids);
-}
-
-
-init() 
+  };
+  Plotly.newPlot('bar', dataa, layout);
+    let trace2 = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        color: otu_ids,
+        size: sample_values
+      }
+  };
+    let data2 = [trace2];
+    let layout2 = {
+      title: 'Belly Button Bubble Chart',
+      showlegend: false,
+      height: 600,
+      width: 600
+  };
+  Plotly.newPlot('bubble', data2, layout2)})}
+  init()
