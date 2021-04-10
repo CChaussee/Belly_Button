0f0c4ce1b6caab9b134b48383a3117c7a268fb4e
Chelsea Chaussee <cfchaussee@gmail.com>
2021-04-10 10:23:25 -0700

    bar plot trace

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,9 +3,17 @@ function data_info(plotmaterial) {
 //Reading in samples.json
       d3.json("samples.json").then (data =>{
           console.log(data)
-          let ids = data.samples.otu_ids;
-          console.log(ids)
+// grabbing data needed for plots          
+          let topids = data.samples.otu_ids.slice(0,10).sort((a,b) => b-a);
+          console.log(topids)
           let sampleValues =  data.samples.sample_values.slice(0,10);
           console.log(sampleValues)
           let labels =  data.samples.otu_labels.slice(0,10);
           console.log (labels)
+// creating bar plot trace
+          let trace1 = {
+            x: sampleValues,
+            y: topids,
+            type: bar,
+            orientation: "h"
+          };
