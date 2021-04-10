63692b42a37699f97c2eac6cf749288c43912ffe
Chelsea Chaussee <cfchaussee@gmail.com>
2021-04-10 10:12:16 -0700

    creating variables for chart making

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,11 @@
 // It only makes sense in my mind to wrtie things in function, sorry
 function data_info(plotmaterial) {
-  //Reading in samples.json
+//Reading in samples.json
       d3.json("samples.json").then (data =>{
           console.log(data)
+          let ids = data.samples.otu_ids;
+          console.log(ids)
+          let sampleValues =  data.samples.sample_values.slice(0,10);
+          console.log(sampleValues)
+          let labels =  data.samples.otu_labels.slice(0,10);
+          console.log (labels)
